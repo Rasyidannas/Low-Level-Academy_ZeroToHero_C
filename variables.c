0be1ec5310dbe2a66a76fb958b9a90d187d52c70
rasyidannas <rasshit.dsgn@gmail.com>
2025-10-06 21:58:51 +0000

    09. variable scope rules

--- variables.c
+++ variables.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+// Global Scope
+int g_NumPersons = 0;
+
+void some_other_function()
+{
+  int personID = 1;
+}
+
 int main()
 {
-  type name = initial value;
+  //type name = initial value;
   int personID = 0;
-  int person2ID = personID + 1;
+  personID += 1;
+  {
+    int personID = 0;
+    personID += 1;
+  }
 }
